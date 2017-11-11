96014d02c5b2898c110f3f12e4f5aafd36f06f7d
dPaulb <31032162+dPaulb@users.noreply.github.com>
2017-11-11 19:05:53 +0900

    Converted to Maven and made jar file

---
+++ src/main/java/sample/Util/PasswordHash.java
@@ -0,0 +1,28 @@
+package sample.Util;
+
+import java.security.MessageDigest;
+
+public class PasswordHash {
+    public String LockPassword(String password) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(password.getBytes("UTF-8"));
+            StringBuffer hexString = new StringBuffer();
+
+            for (int i = 0; i < hash.length; i++) {
+                String hex = Integer.toHexString(0xff & hash[i]);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+
+            return hexString.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+
+    }
+}
